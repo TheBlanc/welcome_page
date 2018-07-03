1a1aa511f56bea6b120401ffc7645f1e453ad6dc
Kevin LeBlanc <YOUR EMAIL ADDRESS>
2018-07-03 13:11:00 -0400

    Add greeting helper method. Add greeting to time show page.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
 
 helper_method :current_background_image
 helper_method :current_user
+helper_method :greeting
+
 
 
 private
@@ -17,6 +19,7 @@ private
     if current_time >= 2000
       #Photo 1
       return "barley.jpeg"
+
     elsif current_time >= 1600
       #Photo 2
       return "dandilion.jpeg"
@@ -39,6 +42,7 @@ private
     end
   end
 
+
   def greeting
     current_time = Time.now.strftime("%H%M").to_i
 
@@ -48,19 +52,11 @@ private
 
     elsif current_time >= 1200
       #Photo 3
-      return "feathers.jpeg"
-
-    elsif current_time >= 800
-      #Photo 4
-      return "waterdrop.jpeg"
-
-    elsif current_time >= 400
-      #Photo 5
-      return "jellyfish.jpeg"
+      return "Good afternoon"
 
     else
-      #Photo 6
-      return "leaves.jpeg"
+      #Photo 4
+      return "Good morning"
     end
   end
 
